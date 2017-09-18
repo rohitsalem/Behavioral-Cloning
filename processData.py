757ca72b36199a4654188acc17fa898f2836a468
rohit <rohitsalem@gmail.com>
2017-09-18 02:21:11 -0700

    Updated processData to generate batches of images

--- processData.py
+++ processData.py
@@ -9,20 +9,18 @@ import csv
 import random
 import numpy as np
 
+BatchSize = 64
 
 # Resize image
 def resize_image(image):
-
     return cv2.resize(image, (200,66), interpolation = cv2.INTER_AREA)
 
 # Normalize the image so that input data values range between -1 and 1
 def normalize_image(image):
-
     return image/127.5-1
 
 # Cropping the image to remove the un-necessary top (sky) and bottom pixels (bonet)
 def crop_image(image):
-
     return image[140:-120,:]
 
 def random_brightness(image):
@@ -38,9 +36,64 @@ def random_brightness(image):
 
 # Combine all the processing for the images: crop->resize->random_brightness
 def process_image(image):
-
      image = crop_image(image)
      image = resize_image(image)
      image = random_brightness(image)
-
      return  image
+
+# Read the CSV file for image location and steering angle
+def get_csv_data(file):
+    image_names, steering_angles = [],[]
+    steering_offset = 0.27 # Offset for left and right cameras
+    with open(file,'r') as f:
+        reader = csv.reader(f)
+        next(reader,None)
+        for left_img, center_img, right_img, steering, _ , _ , _ in reader:
+            angle = float(steering)
+            image_names.append ([left_img.strip() , center_img.strip(), right_img.strip()])
+            steering_angles.append([angle - steering_offset, angle, angle+ steering_offset])
+    return image_names, steering_angles
+
+# Fetch image filenames and steering_angles randomly while limiting the number of straight path(angle=0) data
+def fetch_images(X_train, y_train, batch_size):
+    thresh = 0.1
+    count = 0 # counter for producing #batch_size number of images
+    zero_count = 0
+    images_and_angles = [] # to store image file names and angles
+    while (count < batch_size):
+        index = np.random.ranint(0,len(X_train))
+        angle = y_train[index]
+        image = X_train[index]
+        if(abs(angle)<thresh):
+            if(zero_count<15):
+                images_and_angles.append((image,angle))
+                zero_count = zero_count + 1
+                count = count + 1
+            else:
+                images_and_angles.append((image,angle))
+                count = count + 1
+
+    return images_and_angles
+
+# To generate_batch of images and angles
+def generate_batch(X_train, y_train, batch_size=BatchSize):
+    while True:
+        X_batch = []
+        y_batch = []
+        images_and_angles=fetch_images(X_train,y_train,batch_size)
+        # To read images from the list feteched using the fetch_images function
+        for image_file , angle in images_and_angles:
+            # To ranomly select left/center/right image
+            ind  = np.random.randint(0,3)
+            raw_image = cv2.imread(imPath+image_file[ind])
+            raw_angle = float(angle[ind])
+            # Process the image before yielding the dataset
+            image = process_image(raw_image)
+            # Randomly flipping the image ans steering_angle
+            if random.randrange(2)==1:
+                image = cv2.flip(image,1)
+                raw_angle = -raw_angle
+            X_batch.append(image)
+            y_batch.append(raw_angle)
+        assert len(X_batch) == batch_size, 'len(X_batch) == batch_size should be True'
+        yield np.array(X_batch), np.array(y_batch)
